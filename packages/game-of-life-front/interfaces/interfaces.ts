b37bd53a60374188b0a397724df5d0da95e1097e
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-20 20:24:12 +0200

    feat: add IResponse

--- packages/game-of-life-front/interfaces/interfaces.ts
+++ packages/game-of-life-front/interfaces/interfaces.ts
@@ -1,9 +1,15 @@
-import { HttpStatus } from "@nestjs/common";
-
-export interface IResponseDefault {
-    statusCode: HttpStatus;
-    message: string;
-    error?: string;
-  }
+import { HttpStatus } from '@nestjs/common';
 
 export type Board = number[][];
+
+export interface IResponseDefault {
+  statusCode: HttpStatus;
+  message: string;
+  error?: string;
+}
+
+export interface IResponse {
+  board: Board;
+  id: string;
+  updatedAt: string;
+}
