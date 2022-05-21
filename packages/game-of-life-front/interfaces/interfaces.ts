7197e80adab7d2ecf3904e72968b12252648430e
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-21 22:25:50 +0200

    feat: add ICell, Coordinates

--- packages/game-of-life-front/interfaces/interfaces.ts
+++ packages/game-of-life-front/interfaces/interfaces.ts
@@ -1,6 +1,11 @@
 import { HttpStatus } from '@nestjs/common';
 
-export type Board = number[][];
+export type Board = (0 | 1)[][];
+
+export type Coordinates = {
+  row: number;
+  col: number;
+};
 
 export interface IResponseDefault {
   statusCode: HttpStatus;
@@ -12,4 +17,11 @@ export interface IResponse {
   board: Board;
   id: string;
   updatedAt: string;
+}
+
+export interface ICell {
+  isActive: boolean;
+  row: number;
+  col: number;
+  handleCellClick: (e: Coordinates) => void;
 }
