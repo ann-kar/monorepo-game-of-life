4c6f29a7b9416b2c602ea2ae2a6fe26238a9a5e1
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-22 00:57:55 +0200

    style: style square buttons

--- packages/game-of-life-front/components/Button.tsx
+++ packages/game-of-life-front/components/Button.tsx
@@ -5,13 +5,12 @@ export interface ButtonProps {
   onClick: () => void;
 }
 
-export function Button(props: ButtonProps) {
+export function Button({ label, onClick }: ButtonProps) {
   const [isClicked, setIsClicked] = useState<boolean>(false);
 
   const handleClick = () => {
-    props.onClick();
-    if (props.label !== 'tick' && props.label !== 'restart')
-      setIsClicked(!isClicked);
+    onClick();
+    if (!['tick', 'restart'].includes(label)) setIsClicked(!isClicked);
   };
 
   return (
@@ -19,7 +18,7 @@ export function Button(props: ButtonProps) {
       className="shadow-2xl border border-white first:bg-[#007879] bg-emerald-600 last:bg-[#008875] text-white rounded-none w-20 h-20 uppercase font-semibold text-xs tracking-wider hover:bg-transparent hover:text-emerald-700 active:bg-slate-200 active:text-slate-700 transition-all duration-300"
       onClick={handleClick}
     >
-      {props.label}
+      {label}
     </button>
   );
 }
