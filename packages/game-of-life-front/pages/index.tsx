d757e650b6109c31c7c81c2ba538e98967250a8b
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-22 01:00:15 +0200

    styles: adjust layout, extract SizeInput

--- packages/game-of-life-front/pages/index.tsx
+++ packages/game-of-life-front/pages/index.tsx
@@ -4,9 +4,10 @@ import produce from 'immer';
 import Cell from '../components/cell/cell';
 import { Api } from '../services/services';
 import { Board, Coordinates } from '../interfaces/interfaces';
-import Button from '../components/button/button';
+import Button from '../components/Button';
 import Menu from '../components/menu/menu';
 import { Header } from '../components/Header';
+import { SizeInput } from '../components/SizeInput';
 
 const isBoardEmpty = (board: Board): boolean => {
   return board
@@ -22,21 +23,11 @@ export function Index() {
   const [isEmpty, setIsEmpty] = useState<boolean>(false);
   const [isAutoplayOn, setIsAutoplayOn] = useState<boolean>(false);
   const [boardId, setBoardId] = useState<string>();
-  const [wrongBoardSize, setWrongBoardSize] = useState<boolean>(false);
   const [boardSize, setBoardSize] = useState<number>(10);
 
   useEffect(() => {
     displayBoard(boardSize);
   }, [boardSize]);
-
-  const updateBoardSize = (size: number) => {
-    if (size < 16 && size > 2) {
-      setBoardSize(size);
-      setWrongBoardSize(false);
-    } else {
-      setWrongBoardSize(true);
-    }
-  };
 
   const startGame = async () => {
     const res = await Api.sendBoard(board);
@@ -49,7 +40,7 @@ export function Index() {
     setBoard(res.board);
   };
 
-  const displayBoard = (size:number) => {
+  const displayBoard = (size: number) => {
     const newBoard = new Array(size).fill(0).map((_) => Array(size).fill(0));
     setBoard(newBoard);
   };
@@ -98,65 +89,52 @@ export function Index() {
   }, [isAutoplayOn, isEmpty]);
 
   return (
-    <div className="container mx-auto max-w-md flex flex-col items-center">
+    <div className="container h-screen mx-auto max-w-3xl flex flex-col items-center">
       <Header />
-      <div className="m-2">
-        <label htmlFor="sizeInput">BOARD SIZE:</label>
-        <input
-          id="sizeInput"
-          type="number"
-          placeholder="10"
-          min="3"
-          max="15"
-          onChange={(e) => updateBoardSize(Number(e.target.value))}
-          disabled={hasStarted}
-        />
-        <small>
-          {wrongBoardSize
-            ? 'please provide a number in the range from 3 to 15'
-            : ''}
-        </small>
+      <div className="wrapper flex grow items-center">
+        <div className="board grow shadow-2xl">
+          {board &&
+            board.map((row: number[], rowIndex) => {
+              return (
+                <div className="row flex" key={rowIndex}>
+                  {row.map((cell, colIndex) => {
+                    return (
+                      <Cell
+                        isActive={!!cell}
+                        row={rowIndex}
+                        col={colIndex}
+                        key={`${rowIndex}-${colIndex}`}
+                        handleCellClick={handleCellClick}
+                      />
+                    );
+                  })}
+                </div>
+              );
+            })}
+        </div>
       </div>
-
-      <div className="board">
-        {board &&
-          board.map((row: number[], rowIndex) => {
-            return (
-              <div className="row flex" key={rowIndex}>
-                {row.map((cell, colIndex) => {
-                  return (
-                    <Cell
-                      isActive={!!cell}
-                      row={rowIndex}
-                      col={colIndex}
-                      key={`${rowIndex}-${colIndex}`}
-                      handleCellClick={handleCellClick}
-                    />
-                  );
-                })}
-              </div>
-            );
-          })}
-      </div>
-
-      <nav>
+      <nav className="mt-8 h-40">
         <Menu>
           {hasStarted && (
             <>
-              <Button onClick={tick} label={'tick'} />
+              <Button onClick={tick} label={'next'} />
               <Button onClick={autoplay} label={'autoplay'} />
             </>
           )}
           {!hasStarted && (
             <>
-              <Button onClick={startGame} label={'start'} />
-              <Button onClick={addDefaultPattern} label={'default'} />
+              <Button onClick={startGame} label={'play'} />
+              <Button
+                onClick={addDefaultPattern}
+                label={'add default pattern'}
+              />
             </>
           )}
           <a href="./">
             {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
             <Button onClick={() => {}} label={'restart'} />
           </a>
+          <SizeInput setBoardSize={setBoardSize} hasStarted={hasStarted} />
         </Menu>
       </nav>
     </div>
