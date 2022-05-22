9d215d273052b6ba4c3f74d7aeb479a72a336a6f
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-05-22 20:03:34 +0200

    refactor: create utils with isSizeValid & isBoardEmpty

---
+++ packages/game-of-life-front/utils/utils.ts
@@ -0,0 +1,13 @@
+import { Board } from "../interfaces/interfaces";
+
+export const isSizeValid = (size: number) => {
+  return size < 11 && size > 2;
+};
+
+export const isBoardEmpty = (board: Board): boolean => {
+  return board
+    .map((row) => {
+      return row.every((cell) => cell === 0);
+    })
+    .every((result) => result === true);
+};
