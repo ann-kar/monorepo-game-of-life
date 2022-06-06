8a536c5d7ad6d5fa8653dca27601a8b49bd83396
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-07 00:06:20 +0200

    refactor: add BoardStatus & GameStatus types

--- packages/game-of-life-front/pages/index.tsx
+++ packages/game-of-life-front/pages/index.tsx
@@ -9,15 +9,16 @@ import { Header } from '../components/Header';
 import { SizeInput } from '../components/SizeInput';
 import { isSizeValid, isBoardEmpty } from '../utils/utils';
 
+type BoardStatus = 'empty' | 'filled';
+type GameStatus = 'initialized' | 'inPrep' | 'on' | 'onAutoplay' | 'over';
+
 export function Index() {
+  const [boardStatus, setBoardStatus] = useState<BoardStatus>('empty');
+  const [gameStatus, setGameStatus] = useState<GameStatus>('initialized');
   const [board, setBoard] = useState<Board>();
-  const [prevBoard, setPrevBoard] = useState<Board>();
-  const [hasStarted, setHasStarted] = useState<boolean>(false);
-  const [isEmpty, setIsEmpty] = useState<boolean>(false);
-  const [isAutoplayOn, setIsAutoplayOn] = useState<boolean>(false);
   const [boardId, setBoardId] = useState<string>();
   const [boardSize, setBoardSize] = useState<number>(10);
-  const [isOver, setIsOver] = useState<boolean>(false);
+  const [prevBoard, setPrevBoard] = useState<Board>();
 
   useEffect(() => {
     if (isSizeValid(boardSize)) displayBoard(boardSize);
@@ -25,17 +26,18 @@ export function Index() {
 
   useEffect(() => {
     if (prevBoard && JSON.stringify(board) === JSON.stringify(prevBoard)) {
-      setIsOver(true);
-      setIsAutoplayOn(false);
+      setGameStatus('over');
     }
   }, [board, prevBoard]);
 
   const startGame = async () => {
     if (isSizeValid(boardSize)) {
       const res = await Api.sendBoard(board);
-      setBoard(res.board);
       setBoardId(res.id);
-      setHasStarted(true);
+      setGameStatus('on');
+      const res2 = await Api.sendTick(res.id);
+      setPrevBoard(res.board);
+      setBoard(res2.board);
     }
   };
 
@@ -53,18 +55,19 @@ export function Index() {
 
   useEffect(() => {
     if (board) {
-      isBoardEmpty(board) ? setIsEmpty(true) : setIsEmpty(false);
+      isBoardEmpty(board) ? setBoardStatus('empty') : setBoardStatus('filled');
     }
   }, [board]);
 
   const handleCellClick = (c: ICoordinates) => {
-    if (!hasStarted) {
+    if (gameStatus === 'inPrep' || gameStatus === 'initialized') {
       setBoard(
         produce((draft) => {
           draft[c.row][c.col] = draft[c.row][c.col] === 1 ? 0 : 1;
         })
       );
     }
+    if (gameStatus === 'initialized') setGameStatus('inPrep')
   };
 
   const addDefaultPattern = () => {
@@ -72,7 +75,7 @@ export function Index() {
       .fill(0)
       .map((_) => Array(boardSize).fill(0));
     const leftTop = Math.floor((boardSize - 5) / 2);
-    if (!hasStarted) {
+    if (gameStatus === 'inPrep' || gameStatus === 'initialized') {
       newBoard[leftTop][leftTop + 1] = 1;
       newBoard[leftTop + 1][leftTop + 1] = 1;
       newBoard[leftTop + 1][leftTop + 2] = 1;
@@ -83,34 +86,40 @@ export function Index() {
       newBoard[leftTop + 3][leftTop + 2] = 1;
       newBoard[leftTop + 4][leftTop + 1] = 1;
       setBoard(newBoard);
+      setBoardStatus('filled');
+      setGameStatus('inPrep');
     }
   };
 
   const autoplay = () => {
-    setIsAutoplayOn(!isAutoplayOn);
+    if (gameStatus === 'on') {
+      setGameStatus('onAutoplay');
+    } else if (gameStatus === 'onAutoplay') {
+      setGameStatus('on');
+    }
   };
 
   useEffect(() => {
-    if (isAutoplayOn && !isEmpty) {
+    if (gameStatus === 'onAutoplay' && boardStatus === 'filled') {
       const interval = setInterval(() => {
         tick();
       }, 200);
       return () => clearInterval(interval);
     }
-  }, [isAutoplayOn, isEmpty, tick]);
+  }, [boardStatus, gameStatus, tick]);
 
   return (
     <div className="container h-screen mx-auto max-w-3xl flex flex-col items-center">
       <Header />
       <div className="wrapper flex grow items-center">
-        <div className="board grow shadow-2xl">
+        <div className="board relative grow shadow-2xl">
           {board &&
             board.map((row: number[], rowIndex) => {
               return (
                 <div
-                  className={`row flex ${hasStarted && 'pointer-events-none'} ${
-                    isOver && 'grayscale'
-                  }`}
+                  className={`row   flex ${
+                    (gameStatus !== 'inPrep' && gameStatus !== 'initialized') && 'pointer-events-none'
+                  } ${gameStatus === 'over' && 'grayscale'}`}
                   key={rowIndex}
                 >
                   {row.map((cell, colIndex) => {
@@ -127,26 +136,35 @@ export function Index() {
                 </div>
               );
             })}
+          {gameStatus === 'initialized' && boardStatus === 'empty' && (
+            <div className="absolute top-0 left-0 w-[100%] px-1 leading-10 text-emerald-700 font-bold ">
+              Select some tiles or set the default pattern. Then click START and
+              see what happens!
+            </div>
+          )}
         </div>
       </div>
       <nav className="h-32 flex items-center justify-center">
-        {hasStarted && (
+        {(gameStatus === 'on' || gameStatus === 'onAutoplay') && (
           <>
             <Button onClick={tick} label={'next'} />
             <Button onClick={autoplay} label={'autoplay'} />
           </>
         )}
-        {!hasStarted && (
+        {(gameStatus === 'inPrep' || gameStatus === 'initialized') && (
           <>
-            <Button onClick={startGame} label={'play'} />
-            <Button onClick={addDefaultPattern} label={'add default pattern'} />
+            <Button onClick={startGame} label={'start'} />
+            <Button onClick={addDefaultPattern} label={'set default pattern'} />
           </>
         )}
         <a href="./">
           {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
           <Button onClick={() => {}} label={'restart'} />
         </a>
-        <SizeInput setBoardSize={setBoardSize} hasStarted={hasStarted} />
+        <SizeInput
+          setBoardSize={setBoardSize}
+          isDisabled={(gameStatus === 'inPrep' || gameStatus === 'initialized') ? false : true}
+        />
       </nav>
     </div>
   );
