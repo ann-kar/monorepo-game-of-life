b842a6981db94e2ce53c3caa833b7ec59fe986d8
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-06-06 22:28:28 +0200

    refactor: remove unused console.log

--- packages/game-of-life-front/services/services.ts
+++ packages/game-of-life-front/services/services.ts
@@ -5,14 +5,13 @@ import {
   Board,
 } from '@gol-monorepo/interfaces';
 
-const url = "http://localhost:3333/api";
-// const url =  'https://gol-backend.herokuapp.com/api';
+// const url = "http://localhost:3333/api";
+const url = 'https://gol-backend.herokuapp.com/api';
 
 export class Api {
   static async sendBoard(data: Board): Promise<IResponseStart> {
     try {
       const response = await axios.post(`${url}/board/`, { board: data });
-      console.log("res:", response.data);
       return response.data;
     } catch (err: unknown) {
       throw new Error();
